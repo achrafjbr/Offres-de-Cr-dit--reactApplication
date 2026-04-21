2a9d526c1d96fc75b1e27cb38c5d1e7ca625365a
outeglaout jaouad <outeglaoutjaouad@gmail.com>
2026-04-21 10:16:52 +0200

    initial commit

---
+++ src/utilities/offer-data.js
@@ -0,0 +1,37 @@
+const offers = [
+  {
+    id: "1",
+    provider: "Banque Populaire",
+    amount: 200000,
+    interestRate: 3.5,
+    duration: 20,
+  },
+  {
+    id: "2",
+    provider: "Crédit Agricole",
+    amount: 200000,
+    interestRate: 2.8,
+    duration: 20,
+  },
+  {
+    id: "3",
+    provider: "BNP Paribas",
+    amount: 200000,
+    interestRate: 3.2,
+    duration: 20,
+  },
+  {
+    id: "4",
+    provider: "Société Générale",
+    amount: 200000,
+    interestRate: 3.9,
+    duration: 20,
+  },
+  {
+    id: "5",
+    provider: "CIC",
+    amount: 200000,
+    interestRate: 3.1,
+    duration: 20,
+  },
+];
