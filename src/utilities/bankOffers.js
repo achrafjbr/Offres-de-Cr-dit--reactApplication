eb3976dc091196dbfe14792883d3d83722b70442
outeglaout jaouad <outeglaoutjaouad@gmail.com>
2026-04-21 11:36:17 +0200

    Adding icons needed, and changing offer name

--- src/utilities/bankOffers.js
+++ src/utilities/bankOffers.js
@@ -1,37 +1,37 @@
-const offers = [
+const bankOffers = [
   {
     id: "1",
     provider: "Banque Populaire",
     amount: 200000,
     interestRate: 3.5,
-    duration: 20,
+    year: 2,
   },
   {
     id: "2",
     provider: "Crédit Agricole",
     amount: 200000,
     interestRate: 2.8,
-    duration: 20,
+    year: 3,
   },
   {
     id: "3",
     provider: "BNP Paribas",
     amount: 200000,
     interestRate: 3.2,
-    duration: 20,
+    year: 4,
   },
   {
     id: "4",
     provider: "Société Générale",
     amount: 200000,
     interestRate: 3.9,
-    duration: 20,
+    year: 6,
   },
   {
     id: "5",
     provider: "CIC",
     amount: 200000,
     interestRate: 3.1,
-    duration: 20,
+    year: 7,
   },
 ];
